bc0ac740f8338a5345723d658af1fad1edcf3cd1
philipcha <asd456613@gmail.com>
2017-05-23 21:54:48 +0800

    20170523第一次提交

---
+++ Solution1/Example01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("這是一個範例");
+        }
+    }
+    }
+}
